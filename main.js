30ffcf90c9680588b407eed0fa5423ac8dc1e9f6
HongNyungKi <hnk2017@naver.com>
2020-11-04 19:43:42 +0900

    add

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ btn.addEventListener('click',()=>{
     if(result.value == "수학자"){
         alert('정답입니다~~!!@@ 상금 3마넌!!!')
     }else{
-        alert("땡!!! 친구에게 관심이 없군요? 상금은 알아서 찾으세여~~^^")
+        alert("땡!!! 원우한테 뺨 3대 맞기!!")
     }
 })
