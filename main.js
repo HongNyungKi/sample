a316df2e51fc09c6f39ca81016d00ad4401f9df4
HongNyungKi <hnk2017@naver.com>
2020-11-04 19:34:53 +0900

    add

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
-'use strict';
+const result = document.querySelector('.value');
+const btn = document.querySelector('.btn');
 
-import Navbar from './jsModule/Navbar.js';
-import Home from './jsModule/Home.js';
-import Work from './jsModule/Work.js';
-import ArrowUp from './jsModule/ArrowUp.js';
-
-const navbar = new Navbar();
-const home = new Home();
-const work = new Work();
-const arrowUp = new ArrowUp();
-
-
-navbar.resizeNavbar();
-navbar.clickAutoScroll();
-home.contactAutoScroll();
-home.homeOpacityDown();
-work.controllWorkProject();
-arrowUp.showArrowBtn();
-arrowUp.arrowAutoScroll();
+btn.addEventListener('click',()=>{
+    if(result.value == "수학자"){
+        alert('정답입니다~~!!@@ 상금 3마넌!!!')
+    }else{
+        alert("땡!!! 친구에게 관심이 없군요? 상금은 알아서 찾으세여~~^^")
+    }
+})
